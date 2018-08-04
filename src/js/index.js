40616cb51c69b3218b0d95d17edcfb0b2c703ff7
kristinven <1529908750@qq.com>
2018-08-04 11:51:20 +0800

    modify a bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
+	// 所有的列
 var oLi = document.getElementsByTagName('li');
+	// 获取的图片资源号
 	page = 1;
-	flag =true;
+	// 标记：上一次图片是否已经插入完毕
+	flag = true;
 
 function ajaxFun(){
 	if(flag){
 		flag = false;
-		ajax('get', '/waterfall/getPics.php', getData, 'cpage=' + page, true);
+		// ajax('get', '/waterfall/getPics.php', getData, 'cpage=' + page, true);
+		ajax('get', '/waterfall/data.txt', getData, 'cpage=' + page, true);
 		page++;	
 	}
 }
 ajaxFun();
-// ajax回调函数：
+// ajax回调函数：收到数据后，插入当前长度最短的一列中。
 function getData(data){
 	var value = JSON.parse(data);
-	console.log(value.length);
+	// console.log(value.length);
 	if(value.length > 0){
 		value.forEach(function(ele, index) {
 			var minIndex = minListIndex(oLi);
@@ -33,7 +37,7 @@ function getData(data){
 		flag = true;
 	}
 }
-
+// 查找长度最短的一列
 function minListIndex(dom){
 	var min = dom[0].offsetHeight,
 		minIndex = 0,
@@ -46,7 +50,7 @@ function minListIndex(dom){
 	}
 	return minIndex;
 }
-
+// 滚动触发判断是否获取数据
 window.onscroll = function (){
 	var scrollHeight = document.documentElement.scrollTop || document.body.scrollTop,
 		clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
